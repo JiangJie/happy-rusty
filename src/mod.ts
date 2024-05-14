d486ee36769951a9e1110eabad922bf3d9c4f588
jarvisjiang <jarvisjiang@tencent.com>
2024-05-14 02:04:26 +0000

    add docs for exported symbols

--- src/mod.ts
+++ src/mod.ts
@@ -5,7 +5,23 @@ export { None, Some, type Option } from './enum/option.ts';
 export { Err, Ok, type Result } from './enum/result.ts';
 
 // export some commonly used types
+
+/**
+ * The shorthand of Promise<Option<T>>.
+ */
 export type AsyncOption<T> = Promise<Option<T>>;
+
+/**
+ * The shorthand of Promise<Result<T, E>>.
+ */
 export type AsyncResult<T, E> = Promise<Result<T, E>>;
+
+/**
+ * The shorthand of Result<T, Error>.
+ */
 export type IOResult<T> = Result<T, Error>;
+
+/**
+ * The shorthand of Promise<Result<T, Error>>.
+ */
 export type AsyncIOResult<T> = Promise<IOResult<T>>;
